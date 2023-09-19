7fdb61435e35ed627643d6f60525d73149a7b4da
leidy <leidyjohanasantacruzdaza03@gmail.com>
2023-09-19 17:00:38 -0500

    Initial commit iconos

---
+++ app/src/main/java/com/example/clase18/CourserRVModal.kt
@@ -0,0 +1,8 @@
+package com.gtappdevelopers.kotlingfgproject
+
+data class CourseRVModal(
+    // on below line we are creating a two variable
+    // one for course name and other for course image.
+    var courseName: String,
+    var courseImg: Int
+)
